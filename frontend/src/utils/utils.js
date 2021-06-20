a888006576a55c31907bec112f658ffa9fa88cfd
Voltrein-code <mr-voltrein@yandex.ru>
2021-06-20 03:21:25 +0300

    fix: fix url

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,5 +1,5 @@
 //Объявление переменных
-const serverUrl = 'https://mesto.nomoreparties.co/v1/cohort-21';//адрес сервера
+const serverUrl = 'https://api.voltrein-mesto.nomoredomains.club';//адрес сервера
 const serverToken = 'c717fb8f-7e38-4c7d-b617-f552f6e3b5d3';//токен авторизации
 
 export { serverUrl, serverToken };
